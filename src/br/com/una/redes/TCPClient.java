118dae471e9d489c52208f3970e97d74f21977c7
unknown <31417144@AIMLI323.una.edu.br>
2015-10-30 20:54:02 -0200

    add classes

---
+++ src/br/com/una/redes/TCPClient.java
@@ -0,0 +1,31 @@
+package br.com.una.redes;
+
+import java.io.*; 
+import java.net.*; 
+class TCPClient { 
+
+    public static void main(String argv[ ]) throws Exception 
+    { 
+        String sentence; 
+        String modifiedSentence; 
+        while(true) {
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+
+        Socket clientSocket = new Socket("10.17.6.100", 6789); 
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
+	BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+        
+            sentence = inFromUser.readLine(); 
+
+            outToServer.writeBytes(sentence + '\n');    
+
+            modifiedSentence = inFromServer.readLine(); 
+
+            System.out.println("FROM SERVER: " + modifiedSentence); 
+            clientSocket.close();
+       }
+         
+                   
+    } 
+} 
